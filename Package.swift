0c8b76b4b38197dc7fea5e40aeec1f6112771628
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-02-18 15:19:01 +0300

    Publish swift package YMKStylingAutomotiveNavigation version 4.11.0

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "YMKStylingAutomotiveNavigation",
+    defaultLocalization: "en",
+    platforms: [.iOS("12.0")],
+    products: [
+        .library(
+            name: "YMKStylingAutomotiveNavigation",
+            type: .static,
+            targets: [
+                "YMKStylingAutomotiveNavigation",
+                "YMKStylingAutomotiveNavigationResources"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/yandex/navikit-ios", exact: "4.11.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YMKStylingAutomotiveNavigation",
+            url: "https://maps-ios-pods-public.s3.yandex.net/YMKStylingAutomotiveNavigation-4.11.0.framework.zip",
+            checksum: "994b313629bf78562a9ff64ed2233ca99c1cbce8017d3e617b7aeeaba4a741e2"
+        ),
+        .target(
+            name: "YMKStylingAutomotiveNavigationResources",
+            dependencies: [
+                .product(name: "YandexMapsMobileNavikit", package: "navikit-ios"),
+            ],
+            path: "Resources",
+            resources: [.process("Contents")]
+        )
+    ]
+)
---
+++ Resources/Resources.m
@@ -0,0 +1,13 @@
+#import <resource_bundle_accessor.h>
+
+@interface YMKStylingAutomotiveNavigationSwiftResources : NSObject
+@end
+
+@implementation YMKStylingAutomotiveNavigationSwiftResources
+
++ (NSBundle *)mapkitResourcesBundle
+{
+    return SWIFTPM_MODULE_BUNDLE;
+}
+
+@end
