3e49f480898323f77d1d3daf6786d7fdc81c8715
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-10-10 22:15:02 +0300

    Publish swift package YMKStylingAutomotiveNavigation version 4.24.0

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/yandex/navikit-ios", exact: "4.23.0"),
+        .package(url: "https://github.com/yandex/navikit-ios", exact: "4.24.0"),
     ],
     targets: [
         .binaryTarget(
             name: "YMKStylingAutomotiveNavigation",
-            url: "https://maps-ios-pods-public.s3.yandex.net/YMKStylingAutomotiveNavigation-4.23.0.framework.zip",
-            checksum: "fecd3723bcaaa5c4605b8ac747e4ae4e0be97b247c201db75ac4bcd1b99c5681"
+            url: "https://maps-ios-pods-public.s3.yandex.net/YMKStylingAutomotiveNavigation-4.24.0.framework.zip",
+            checksum: "2611bfe1aa18af7effebd7382198cb9e0747435739a14630870f064fa5142c8c"
         ),
         .target(
             name: "YMKStylingAutomotiveNavigationResources",
